9837ef01f9390075a2b1842f81fc22cc145f1d82
Francine <francine.bergamin@gmail.com>
2022-10-01 14:00:22 -0300

    Responsive feature

--- dshape/resources/views/home.blade.php
+++ dshape/resources/views/home.blade.php
@@ -5,137 +5,85 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/tailwind.css')
 
+  <!-- FA Icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
   <!-- Raleway -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100&family=Raleway:ital,wght@0,600;0,700;0,800;0,900;1,900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@100&family=Raleway:ital,wght@0,400;0,600;0,700;0,800;0,900;1,900&display=swap" rel="stylesheet">
 
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <title>DShape</title>
 </head>
 
-
-<body class="overflow-hidden bg-fixed bg-center mx-20 font-raleway w-screen h-screen bg-gradient-to-tl from-[#000000] to-[#13101B] bg-cover bg-no-repeat">
-
-  <!-- logo -->
-  <header class="
-    flex
-    mb-8
-    h-40
-    max-h-40
-    items-center
-    justify-start
-    gap-8">
-
-    <p class="text-zinc-100 text-3xl">logo de milhõe$$</p>
+<body class="font-raleway max-h-screen max-w-screen bg-cover dark:bg-stone-900 bg-zinc-200">
+  <header class="z-50 flex justify-center top-0 py-8 fixed w-full h-auto bg-stone-900 bg-opacity-95 backdrop-blur-sm">
+    <nav id="menu">
+      <ul class="inline-flex gap-28 tracking-wider uppercase dark:text-zinc-100 text-neutral-900 text-2xl">
+        <li class="w-fit m-0 p-0"><a href="#" class="hover:text-green-500">Início</a></li>
+        <li class="w-fit m-0 p-0"><a href="#" class="hover:text-green-500">Como funciona</a></li>
+        <li class="w-fit m-0 p-0"><a href="#" class="hover:text-green-500">Recomendar</a></li>
+      </ul>
+    </nav>
   </header>
 
-  <!-- <svg class="z-10 overflow-hidden flex w-auto fixed md:hidden sm:hidden" viewBox="0 250 610 1080" xmlns="http://www.w3.org/2000/svg">
-    <path d="M0 1280L550.835 0H782V1080H0Z" fill="#B04AFF" />
-    <path d="M194 1080L600.909 0H893V1080H194Z" fill="#8F47EA" />
-    <path d="M400 900L616.791 0H910V1080H400Z" fill="#8441DA" />
-  </svg> -->
+  <main class="mt-32 m-10 sm:m-32">
 
-  <main class="z-20  flex flex-col justify-start w-auto h-auto">
-    <!-- título e subtítulo -->
-    <div class="">
-      <h2 class="
-        text-8xl
-        text-transparent
-        bg-clip-text
-        font-black
-        uppercase
-        bg-gradient-to-tl
-        from-purple-700
-        to-purple-500">
-        DShape
-      </h2>
+    <section id="introductionWrapper" class="flex-col lg:flex-row flex items-center sm:gap-8">
+      <section id="ad" class="flex-2 text-center w-full">
 
-      <h3 class="
-        text-6xl
-        uppercase
-        italic
-        tracking-[0.3em]
-        text-transparent
-        bg-clip-text
-        font-black
-        bg-gradient-to-r
-        from-emerald-400
-        to-violet-500">
-        Fala por mim
-      </h3>
-    </div>
+          <h1 class="text-6xl sm:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-lime-400 to-green-500 italic tracking-wider uppercase font-black mb-2">
+            Dshape
+          </h1>
 
-    <!-- frase motivacional -->
-    <div class="mt-24 text-zinc-100 leading-[134.9%] text-[2rem]">
-      <p>O maior prazer na vida é fazer o que as</p>
-      <p>pessoas falam que você não consegue.</p>
-    </div>
+        <blockquote class="w-auto mb-12 text-4xl text-slate-100 font-bold">
+          O que não te mata, te faz mais
+          <span class="text-center mt-2 sm:text-start before:block before:absolute before:-inset-0.5 before:-skew-y-2 px-2 before:bg-green-500 relative inline-block">
+            <span class="relative dark:text-neutral-900 text-slate-100">forte!</span>
+          </span>
+        </blockquote>
 
-    <!-- formulário de cadastro -->
-      <div class="flex text-zinc-100">
-        <p class="mt-14 text-3xl">Conquiste isso com o <span class="font-extrabold text-purple-700">Dshape</span></p>
-      </div>
+        <p class="dark:text-zinc-100 text-neutral-900 text-md 2xl:text-2xl">
+          Lorem ipsum dolor sit amet. Ad veritatis molestias voluptas
+          tempore et voluptatibus suscipit qui reiciendis beatae.
+          Quo voluptatem enim qui modi totam quo voluptas impedit
+          et quia harum aut animi vero et sapiente galisum.
+          Et pariatur repellendus id consectetur vero et dolores lorem
+          voluptas et repellendus dolorem in molestiae veniam.
+        </p>
+      </section>
 
-      <!-- input -->
-      <form name="singup" action="">
-        <div class="flex flex-col font-gothic font-semibold pt-8">
-          <input class="
-          p-5
-          max-w-xl
-          max-h-14
-          rounded-sm
-          text-lg
-          placeholder:text-lg
-          placeholder:text-zinc-700" type="email" placeholder="Email">
+      <section id="rightImg" class="flex-2 m-0">
+        <img class="dark:invert w-full h-full object-cover hidden lg:block" src="{{ asset('img/monstro.svg') }}">
+      </section>
+    </section>
 
-          <input class="
-          mt-3
-          p-5
-          max-w-xl
-          h-14
-          rounded-sm
-          text-lg
-          placeholder:text-lg
-          placeholder:text-zinc-700" type="password" placeholder="Senha">
+    <section id="explanationWrapper" class="flex flex-col mt-28">
 
-          <button type="submit" class="
-          mt-8
-          uppercase
-          text-xl
-          backdrop-blur-sm
-          border-opacity-70
-          border-[0.2rem]
-          border-zinc-900
-          bg-opacity-10
-          tracking-widest
-          text-zinc-100
-          rounded-sm
-          bg-gradient-to-t
-          from-purple-700
-          to-purple-500
-          font-regular
-          w-fit
-          h-fit
-          py-4
-          px-12">
-            Virar uma lenda!</button>
-        </div>
+      <section id="explanation" class="flex-1">
+        <p class="dark:text-zinc-100 text-neutral-900 font-normal text-2xl text-center">
+          Lorem ipsum dolor sit amet. Ad veritatis molestias voluptas
+          tempore et voluptatibus suscipit qui reiciendis beatae.
+          Quo voluptatem enim qui modi totam quo voluptas impedit
+          et quia harum aut animi vero et sapiente galisum.
+          Et pariatur repellendus id consectetur vero et dolores lorem
+          voluptas et repellendus dolorem in molestiae veniam.
+        </p>
+      </section>
 
+    </section>
 
-    </form>
-
-
+    <section id="cadastro"></section>
   </main>
 
-  <!-- seta para descer -->
-  <footer>
+  <footer></footer>
 
-  </footer>
-
-
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 
 </html>
